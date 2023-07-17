e1c69cc39b88d58a924c01f19ccbe986abc9402a
Fidan Bashirova <fidanbb@code.edu.az>
2023-07-17 20:32:27 +0400

    added classWork and homework

---
+++ ClassWorkHeapStackAndOverloadMethods/ClassWorkHeapStackAndOverloadMethods/Program.cs
@@ -0,0 +1,220 @@
+﻿#region ClassWork
+//static void GetFullData(string name,string surname,int age,string address="Ehmedli")
+//{
+//    string result = $"Name: {name}, Surname: {surname},Age: {age}, Address: {address}";
+//    Console.WriteLine(result);
+//}
+
+//GetFullData("Sheref","Tenzilli",26);
+
+
+
+
+//static void GetNumsOfArray(int number,int num,params int[] arr)
+//{
+//    foreach (var item in arr)
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+//int[] numbers = { 2, 3, 5, 6, 7 };
+
+//GetNumsOfArray(1,2,3,4,5);
+
+
+
+//int a = 5;
+//int b = a;
+//b = 100;
+
+//Console.WriteLine(a);
+
+//Console.WriteLine(b);
+
+
+
+
+//int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+
+//Console.WriteLine(arr1[0]);
+
+//int [] arr2 = arr1;
+
+//arr2[0] = 100;
+
+//Console.WriteLine(arr1[0]);
+//Console.WriteLine(arr1[0]);
+
+
+
+//int a = 8;
+
+//Console.WriteLine(a);
+
+//Test(a);
+
+
+//static void Test(int a)
+//{
+//    a = 20;
+
+//    Console.WriteLine(a);
+//}
+
+
+
+//int a = 8;
+
+
+//Test(ref a);
+
+//Console.WriteLine(a);
+
+
+//static void Test(ref int a)
+//{
+//    a = 20;
+
+//    Console.WriteLine(a);
+//}
+
+
+//int a ;
+
+
+//Test(out a);
+
+//Console.WriteLine(a);
+
+
+//static void Test(out int a)
+//{
+//    a = 20;
+
+//    Console.WriteLine(a);
+//}
+
+
+//static void GetFullName(string name,string surname)
+//{
+//    Console.WriteLine(name + " " + surname);
+//}
+
+
+using System.Text;
+using ClassWorkHeapStackAndOverloadMethods;
+
+//Class1.Test();
+
+//Class1.Search("sdahg");
+
+
+//StringBuilder stringBuilder = new StringBuilder("Salam");
+
+//stringBuilder.Append("- Azerbaycan");
+
+//Console.WriteLine(stringBuilder);
+
+
+
+
+
+//string name = new String("salamlar");
+
+//string surname = " ";
+
+//Console.WriteLine(surname);
+
+
+
+//string name = "Samir";
+
+//Test(ref name);
+
+//Console.WriteLine(name);
+
+//static void Test ( ref string name)
+//{
+//    name = "Xeyyam";
+//    Console.WriteLine(name);
+//}
+
+
+
+
+//Console.WriteLine("A".ToLower() == "a".ToLower());
+
+string name = "Salam Azerbaycan";
+
+//Console.WriteLine(name.Length);
+
+//Console.WriteLine(name.ToLower());
+//Console.WriteLine(name.Trim());
+
+//Console.WriteLine(name.Replace("e","a"));
+
+
+//for (int i = 0; i < name.Length; i++)
+//{
+//    Console.WriteLine(name[i]);
+//}
+
+
+//var arr = name.Split(" ");
+
+//string result = arr.ToString();
+
+//foreach (var item in arr)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//string str1 = "Programmingdgsakgahfvjbgfbjbbnfgkjbjf";
+
+//string str2 = "Programming";
+
+//Console.WriteLine(str1+str2);
+
+//string result = string.Concat(str1,str2);
+
+//Console.WriteLine(result);
+
+//if (str1==str2)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+
+//bool isEqual = str1.Equals(str2);
+//Console.WriteLine(isEqual);
+
+//Console.WriteLine(str1.Substring(0,10) + " ......");
+
+//string surname = "Filankesov";
+//Console.WriteLine(surname.Contains("s"));
+
+
+
+
+//string text = "salam";
+
+//ReplaceSpecificCharFromStr(text, 3, 'c');
+
+//static void ReplaceSpecificCharFromStr(string str, int index, char replaceChar)
+//{
+//    var arr = str.ToCharArray();
+
+//    arr[index] = replaceChar;
+
+//    arr.ToString();
+
+//    Console.WriteLine(arr);
+//}
+
+#endregion
+
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿
+#region 2. Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin. (Cavid gelirse , ekranda divaC gorsensin).
+
+//Console.WriteLine("Enter a string: ");
+
+//string str = Console.ReadLine();
+
+//ShowViceVerseOfString(str);
+
+
+//static void ShowViceVerseOfString(string txt)
+//{
+//    for (int i = txt.Length-1; i >=0 ; i--)
+//    {
+//        Console.WriteLine(txt[i]);
+//    }
+//}
+
+
+#endregion
+
+
+#region 3. String qebul eden bir method yazirsiz. Hemin method qebul etdiyi stringin ilk herfini boyuk yazdirmalidir. (salam gelirse ekranda Salam gostermelidir)
+
+//ShowFirstCharAsUpperCase("fidan");
+
+//static void ShowFirstCharAsUpperCase(string str)
+//{
+//    Console.WriteLine(char.ToUpper(str[0]) + str.Substring(1));
+
+
+
+//}
+
+
+#endregion
